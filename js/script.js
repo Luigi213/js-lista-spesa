1db5e92704b8457325e6c27c2bf06dad4dc0bc0f
Luigi213 <reyesgianluigi88@gmail.com>
2022-12-02 13:48:36 +0100

    Setup start

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Creare spesaArrey per la lista della spesa
+const spesaArrey = [
+    'banana',
+    'kebab',
+    'pollo',
+    'pizza',
+    'pasta',
+    'acqua',
+    'succo',
+    'riso'
+];
+
+
