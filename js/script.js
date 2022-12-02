d797f743757a41dd6fcceae6d7b26f84156f16b3
Luigi213 <reyesgianluigi88@gmail.com>
2022-12-02 14:11:36 +0100

    add the comment

--- js/script.js
+++ js/script.js
@@ -9,9 +9,12 @@ const spesaArray = [
     'succo',
     'riso'
 ];
+// stampo lista
+// riporto il DOM
 let container = document.querySelector('.lista');
 let i = 0;
 let list = '';
+// Creo la il loop per poi crearmi la lista
 while(i < spesaArray.length){
     list += `<li>${spesaArray[i]}</li>`
     container.innerHTML = list
